89688bc1f7d2e44f210e7ed11efdfc7bb23c0d1d
Henrik005 <henrik.mathias.brathen@gmail.com>
2025-01-03 14:37:26 +0100

    Add project files.

---
+++ CsharpSocialMediaOppgave/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpSocialMediaOppgave
+{
+    internal class User
+    {
+        string _name;
+        int _password;
+        //List of Friends.
+        string _email;
+        int _phone;
+        string _about;
+        //Maybe more stuff related to a user down here too, idk.
+    }
+}
